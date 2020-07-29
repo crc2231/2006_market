7d2d7c0f235beafe590974c6e9b34efa9de6540c
Corey Carter <crc2231@gmail.com>
2020-07-29 10:03:24 -0600

    Iteration 1 complete

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,7 +1,26 @@
 class Vendor
-  attr_reader :name,
+  attr_reader :name, :inventory
 
   def initialize(name)
     @name = name
     @inventory = {}
   end
+
+  def check_stock(item)
+      if @inventory.include?(item)
+        stock_count = @inventory.fetch(item)
+        stock_count
+      else
+        0
+      end
+  end
+
+  def stock(item, quantity)
+    if @inventory[item] == nil
+        @inventory[item] = quantity
+    else
+      @inventory[item] += quantity
+    end
+  end
+
+end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -2,14 +2,47 @@ require "minitest/autorun"
 require "minitest/pride"
 require "mocha/minitest"
 require "./lib/vendor"
-
+require "./lib/item"
 class VendorTest < Minitest::Test
 
+  # def setup
+  #   vendor = Vendor.new("Rocky Mountain Fresh")
+  #   item1 = Item.new({name: 'Peach', price: "$0.75"})
+  #   item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  #
+  #
+  # end
+
   def test_it_exists
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
     assert_instance_of Vendor, vendor
   end
 
   def test_it_has_attributes
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    assert_equal "Rocky Mountain Fresh", vendor.name
+
+    expected = {}
+    assert_equal expected, vendor.inventory
+  end
+
+  def test_check_stock_starts_zero
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    assert_equal 0, vendor.check_stock(item1)
+  end
+
+  def test_check_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor.stock(item1, 30)
+    assert_equal 30, vendor.check_stock(item1)
   end
 
 end
